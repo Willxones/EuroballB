ae567681c751f7c390957969ba69232c2830f844
Willxones <william.jones@hl.co.uk>
2024-06-23 20:45:05 +0100

    File structure and react router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/home'
+
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+      <div>
+        <section>                              
+            <Routes>                                                                        
+              <Route path='/' Component={Home}/>
+            </Routes>                    
+        </section>
+      </div>
+    </Router>
   )
 }
 
